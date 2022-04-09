f3f0529c3b5002cb87e259fe45c5e754888de9e9
AbhishekSingh612 <67855380+AbhishekSingh612@users.noreply.github.com>
2022-04-10 00:26:58 +0530

    fixed cors issue

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/<user>/profile', methods=['GET'])
 def hello_world(user):
@@ -8,6 +10,7 @@ def hello_world(user):
 
 
 @app.route('/', methods=['POST'])
+@cross_origin()
 def hello_post():
     user = request.form['nm']
     return '<h1>Hello GET </h1><br>' + user
